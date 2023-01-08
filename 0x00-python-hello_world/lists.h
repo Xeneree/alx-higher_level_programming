3096cc842641a91b2646992900e6c795a3b750e7
Xeneree <xeneree@gmail.com>
2023-01-08 09:35:18 +0000

    Start Afresh

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,8 +1,7 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
-
 
 typedef struct listint_s
 {
@@ -15,4 +14,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif
